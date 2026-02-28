fc3c3ed00e761f898fc88bdf5d8b1e0b7a305f13
Bhargavan-BM <56824743+Bhargavan-BM@users.noreply.github.com>
2026-03-01 01:31:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: "/Kaizentrix/",  // 👈 IMPORTANT for GitHub Pages
+  base: '/',  // 👈 IMPORTANT for GitHub Pages
 
   plugins: [
     react(),
