7a14f6749a7a9cf94fb21db959165dbf2b2bf1de
Bhargavan-BM <Bhargavan.bm@gmail.com>
2026-02-28 18:26:12 +0530

    Add HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";   // 👈 ADD THIS
 import App from "./app/App";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <HashRouter>   {/* 👈 WRAP APP */}
+    <App />
+  </HashRouter>
 );
